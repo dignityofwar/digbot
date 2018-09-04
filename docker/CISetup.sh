f0fc68bdc6939d731ede17efbf24383c70d5520d
unknown <jameslongman2@gmail.com>
2018-09-04 13:14:30 +0100

    test of CISetup.sh use

--- docker/CISetup.sh
+++ docker/CISetup.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 configFile=$1;
+token=$2;
 
 node > ${configFile} <<EOF
-//Read data
 var config = require('./${configFile}');
 
-//Manipulate data
-
-//Output data
-console.log("====TEST LOG====")
-console.log(config.environment);
+// Insert environment variables for testing
+config.token = ${token};
 
 EOF
