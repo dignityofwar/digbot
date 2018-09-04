7a1e31211b1e0f2c1c11310fa8ba910008e4373f
unknown <jameslongman2@gmail.com>
2018-09-04 18:35:07 +0100

    test of CISetup.sh use

--- docker/CISetup.sh
+++ docker/CISetup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-configFile="../config/envConfig.json";
-token=$1;
+configFile="/home/travis/build/JamesLongman/DIGBot/config/envConfig.json"
+token=$1
 
 # Rewrite enviroment variables
 jq -c '.token = $newVal' --arg newVal $token $configFile > tmp.$$.json && mv tmp.$$.json $configFile
