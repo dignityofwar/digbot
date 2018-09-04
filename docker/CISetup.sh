d626bef8b33909fb3b1e7515b17a461117a3bc00
unknown <jameslongman2@gmail.com>
2018-09-04 18:06:35 +0100

    test of CISetup.sh use

--- docker/CISetup.sh
+++ docker/CISetup.sh
@@ -2,13 +2,5 @@
 configFile=$1;
 token=$2;
 
-node > ${configFile} <<EOF
-var config = require('${configFile}');
-
-// Define environment
-config.environment = 'development';
-
-// Insert environment variables for testing
-config.token = '${token}';
-
-EOF
+# Rewrite enviroment variables
+jq -c '.token = ${token}' configFile > tmp.$$.json && mv tmp.$$.json configFile
