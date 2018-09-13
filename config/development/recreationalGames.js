2f30546f759aa4e593cd113d6a91a10c22798f7b
unknown <jameslongman2@gmail.com>
2018-09-13 21:14:20 +0100

    added additional recreational games to test server

--- config/development/recreationalGames.js
+++ config/development/recreationalGames.js
@@ -10,6 +10,20 @@
                 '486335986628821003'
             ],
             primaryChannel: '486324305915936769'
+        },
+        EU4: {
+            name: 'Europa Universalis 4',
+            roleids: [
+                '486335986628821003'
+            ],
+            primaryChannel: '486324305915936769'
+        },
+        GuildWars2: {
+            name: 'Guild Wars 2',
+            roleids: [
+                '318341912840896515'
+            ],
+            primaryChannel: '489880473699483659'
         }
     };
 }());
