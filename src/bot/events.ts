b4e2140333c44639446552d67227f501ddadefac
Microwavekonijn <sjorsw@protonmail.ch>
2020-03-03 11:47:22 +0100

    Update DiscordEvents to discord.js v12

--- src/bot/events.ts
+++ src/bot/events.ts
@@ -1,33 +1,35 @@
 import {
     Channel,
-    ClientUserGuildSettings,
-    ClientUserSettings,
     Collection,
-    Emoji,
+    DMChannel,
     Guild,
+    GuildChannel,
+    GuildEmoji,
     GuildMember,
     Message,
     MessageReaction,
+    Presence,
     Role,
     Snowflake,
+    Speaking,
     TextChannel,
     User,
+    VoiceState,
 } from 'discord.js';
+import { CloseEvent } from 'ws';
 
 /**
  * Types for the various event send from Discord
+ *
  */
-export type onChannelCreate = (channel: Channel) => void;
-export type onChannelDelete = (channel: Channel) => void;
-export type onChannelPinsUpdate = (channel: Channel, time: Date) => void;
-export type onChannelUpdate = (oldChannel: Channel, newChannel: Channel) => void;
-export type onClientUserGuildSettingsUpdate = (settings: ClientUserGuildSettings) => void;
-export type onClientUserSettingsUpdate = (settings: ClientUserSettings) => void;
+export type onChannelCreate = (channel: DMChannel | GuildChannel) => void;
+export type onChannelDelete = (channel: DMChannel | GuildChannel) => void;
+export type onChannelPinsUpdate = (channel: DMChannel | GuildChannel, time: Date) => void;
+export type onChannelUpdate = (oldChannel: DMChannel | GuildChannel, newChannel: DMChannel | GuildChannel) => void;
 export type onDebug = (info: string) => void;
-export type onDisconnect = (event: CloseEvent) => void;
-export type onEmojiCreate = (emoji: Emoji) => void;
-export type onEmojiDelete = (emoji: Emoji) => void;
-export type onEmojiUpdate = (oldEmoji: Emoji, newEmoji: Emoji) => void;
+export type onEmojiCreate = (emoji: GuildEmoji) => void;
+export type onEmojiDelete = (emoji: GuildEmoji) => void;
+export type onEmojiUpdate = (oldEmoji: GuildEmoji, newEmoji: GuildEmoji) => void;
 export type onError = (error: Error) => void;
 export type onGuildBanAdd = (guild: Guild, user: User) => void;
 export type onGuildBanRemove = (guild: Guild, user: User) => void;
@@ -35,33 +37,36 @@ export type onGuildCreate = (guild: Guild) => void;
 export type onGuildDelete = (guild: Guild) => void;
 export type onGuildIntegrationsUpdate = (guild: Guild) => void;
 export type onGuildMemberAdd = (member: GuildMember) => void;
-export type onGuildMemberAvailable = (member: GuildMember) => void;
 export type onGuildMemberRemove = (member: GuildMember) => void;
-export type onGuildMembersChunk = (members: GuildMember[], guild: Guild) => void;
-export type onGuildMemberSpeaking = (member: GuildMember, speaking: boolean) => void;
+export type onGuildMembersChunk = (members: Collection<Snowflake, GuildMember>, guild: Guild) => void;
+export type onGuildMemberSpeaking = (member: GuildMember, speaking: Readonly<Speaking>) => void;
 export type onGuildMemberUpdate = (oldMember: GuildMember, newMember: GuildMember) => void;
 export type onGuildUnavailable = (guild: Guild) => void;
 export type onGuildUpdate = (oldGuild: Guild, newGuild: Guild) => void;
+export type onInvalidated = () => void;
 export type onMessage = (message: Message) => void;
 export type onMessageDelete = (message: Message) => void;
 export type onMessageDeleteBulk = (messages: Collection<Snowflake, Message>) => void;
 export type onMessageReactionAdd = (reaction: MessageReaction, user: User) => void;
 export type onMessageReactionRemove = (reaction: MessageReaction, user: User) => void;
 export type onMessageReactionRemoveAll = (message: Message) => void;
+export type onMessageReactionRemoveEmoji = (reaction: MessageReaction) => void;
 export type onMessageUpdate = (oldMessage: Message, newMessage: Message) => void;
-export type onPresenceUpdate = (oldMessage: Message, newMessage: Message) => void;
-export type onRateLimit = (info: object, limit: number, timeDiff: number, path: string, method: string) => void;
+export type onPresenceUpdate = (oldPresence: Presence | null, newPresence: Presence) => void;
+export type onRateLimit = (info: { timeout: number; limit: number; method: string; path: string; route: string }) => void;
 export type onReady = () => void;
-export type onReconnecting = () => void;
-export type onResume = (replayed: number) => void;
 export type onRoleCreate = (role: Role) => void;
 export type onRoleDelete = (role: Role) => void;
 export type onRoleUpdate = (oldRole: Role, newRole: Role) => void;
+export type onShardDisconnect = (event: CloseEvent, id: number) => void;
+export type onShardError = (error: Error, id: number) => void;
+export type onShardReady = (id: number, unavailableGuilds?: Set<string>) => void;
+export type onShardReconnecting = (id: number) => void;
+export type onShardResume = (id: number, replayedEvents: number) => void;
 export type onTypingStart = (channel: Channel, user: User) => void;
 export type onTypingStop = (channel: Channel, user: User) => void;
-export type onUserNoteUpdate = (user: User, oldNote: string, newNote: string) => void;
 export type onUserUpdate = (oldUser: User, newUser: User) => void;
-export type onVoiceStateUpdate = (oldMember: GuildMember, newMember: GuildMember) => void;
+export type onVoiceStateUpdate = (oldState: VoiceState, newState: VoiceState) => void;
 export type onWarn = (info: string) => void;
 export type onWebhookUpdate = (channel: TextChannel) => void;
 
@@ -69,10 +74,7 @@ export type discordEvent = onChannelCreate
     | onChannelDelete
     | onChannelPinsUpdate
     | onChannelUpdate
-    | onClientUserGuildSettingsUpdate
-    | onClientUserSettingsUpdate
     | onDebug
-    | onDisconnect
     | onEmojiCreate
     | onEmojiDelete
     | onEmojiUpdate
@@ -83,31 +85,34 @@ export type discordEvent = onChannelCreate
     | onGuildDelete
     | onGuildIntegrationsUpdate
     | onGuildMemberAdd
-    | onGuildMemberAvailable
     | onGuildMemberRemove
     | onGuildMembersChunk
     | onGuildMemberSpeaking
     | onGuildMemberUpdate
     | onGuildUnavailable
     | onGuildUpdate
+    | onInvalidated
     | onMessage
     | onMessageDelete
     | onMessageDeleteBulk
     | onMessageReactionAdd
     | onMessageReactionRemove
     | onMessageReactionRemoveAll
+    | onMessageReactionRemoveEmoji
     | onMessageUpdate
     | onPresenceUpdate
     | onRateLimit
     | onReady
-    | onReconnecting
-    | onResume
     | onRoleCreate
     | onRoleDelete
     | onRoleUpdate
+    | onShardDisconnect
+    | onShardError
+    | onShardReady
+    | onShardReconnecting
+    | onShardResume
     | onTypingStart
     | onTypingStop
-    | onUserNoteUpdate
     | onUserUpdate
     | onVoiceStateUpdate
     | onWarn
