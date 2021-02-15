a1a0ed37b77dac9475e0349b86567e5e378e5207
Microwavekonijn <sjorsw@protonmail.ch>
2021-02-15 13:25:23 +0100

    Added support for mariadb

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,30 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-// require('dotenv').config();
-
-module.exports = {
-    type: 'sqlite',
-    database: process.env.DB_DATABASE || './storage/db.sqlite',
+const settings = {
     entities: [
         `${__dirname}/dist/**/*.entity.js`,
     ],
     synchronize: true,
     // migrationsRun: true,
 };
+
+module.exports = ((connection) => {
+    switch (connection) {
+        case 'sqlite':
+            return {
+                type: 'sqlite',
+                database: process.env.DB_DATABASE || './storage/db.sqlite',
+                ...settings
+            }
+        case 'mariadb':
+            return {
+                type: 'mariadb',
+                host: process.env.DB_HOST || 'localhost',
+                port: process.env.DB_PORT || '3306',
+                database: process.env.DB_DATABASE || 'digbot',
+                username: process.env.DB_USER,
+                password: process.env.DB_PASS,
+                ...settings
+            }
+        default:
+            throw new Error(`Database connection "${connection}" not recognized`);
+    }
+})(process.env.DB_CONNECTION);
